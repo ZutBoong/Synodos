0609fb87e78503e9d037ab1334b168379ec2ca92
Fafner <alex2000926@naver.com>
2026-01-06 10:43:44 +0900

    final

--- frontend/src/pages/views/FilesView.js
+++ frontend/src/pages/views/FilesView.js
@@ -119,14 +119,14 @@ function FilesView({ team, teamMembers, loginMember, filters }) {
                                         onClick={() => handleDownload(file)}
                                         title="다운로드"
                                     >
-                                        <i className="fa-solid fa-download"></i>
+                                        <i className="fa-solid fa-arrow-down"></i>
                                     </button>
                                     <button
                                         className="delete-btn"
                                         onClick={() => handleDelete(file.fileId)}
                                         title="삭제"
                                     >
-                                        <i className="fa-solid fa-trash"></i>
+                                        <i className="fa-solid fa-xmark"></i>
                                     </button>
                                 </div>
                             </div>
